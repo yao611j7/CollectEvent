5ba574729da112522a5e51381693ac247fa51193
Kelly <yao611j7@gmailc.om>
2019-11-18 23:40:47 -0500

    add intent to start event add activity from Login

--- app/src/main/java/laioffer/com/Event.java
+++ app/src/main/java/laioffer/com/Event.java
@@ -10,6 +10,12 @@ public class Event {
     /**
      * Constructor
      */
+    private int like;
+    private String id;
+    private long time;
+    private String username;
+    private String imgUri;
+    public Event() {}
     public Event(String title, String address, String description) {
         this.title = title;
         this.address = address;
@@ -21,4 +27,56 @@ public class Event {
     public String getTitle() { return this.title; }
     public String getAddress() { return this.address; }
     public String getDescription() { return this.description; }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getLike() {
+        return like;
+    }
+
+    public void setLike(int like) {
+        this.like = like;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public long getTime() {
+        return time;
+    }
+
+    public void setTime(long time) {
+        this.time = time;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getImgUri() {
+        return imgUri;
+    }
+
+    public void setImgUri(String imgUri) {
+        this.imgUri = imgUri;
+    }
 }
--- app/src/main/java/laioffer/com/EventActivity.java
+++ app/src/main/java/laioffer/com/EventActivity.java
@@ -1,14 +1,20 @@
 package laioffer.com;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 
 public class EventActivity extends AppCompatActivity {
-
+    private Fragment mEventsFragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event);
+        if (mEventsFragment == null) {
+            mEventsFragment = new EventsFragment();
+        }
+        getSupportFragmentManager().beginTransaction().
+                add(R.id.relativelayout_event, mEventsFragment).commit();
     }
 }
